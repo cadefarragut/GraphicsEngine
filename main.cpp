6c26b85ebb9756cbc9cca5789d627c29ada8da6e
cadefarragut <cade_farragut@yahoo.com>
2026-08-12 23:11:12 -0500

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(1920, 1080, "personalproj", NULL, NULL);
+
+	if (window == NULL) {
+		std::cout << "Failed to create window" << std::endl;
+		glfwTerminate();
+		return 1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout<< "Failed to initialize GLAD" << std::endl;
+		return 1;
+	}
+
+	glViewport(0, 0, 1920, 1080);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate(); 
+	return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+///
+/// Function Name: frambuffer_size_callback
+/// Summary: Function is called when we want to change the size of our window
+/// 
+//////////////////////////////////////////////////////////////////////////////
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
